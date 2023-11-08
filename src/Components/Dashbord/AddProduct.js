0bc359435e0322daa719b3e4dbdab415c8d58af4
Md Asaduzaman <asadasad9282@gmail.com>
2023-11-08 21:38:57 +0600

    changes

--- src/Components/Dashbord/AddProduct.js
+++ src/Components/Dashbord/AddProduct.js
@@ -1,36 +1,62 @@
-import React from 'react';
+import React from "react";
 
 const AddProduct = () => {
+  const handelFormSubmit = () => {};
 
-    const handelFormSubmit = () => {
+  return (
+    <div>
+      <div>
+        <div className=''>
+          <h3 className='font-bold text-2xl text-center text-secondary'>
+            {"Add New Product"}
+          </h3>
 
-    }
+          <form
+            onSubmit={handelFormSubmit}
+            className='grid grid-cols-1 gap-5 mt-4 justify-items-center'
+          >
+            <input
+              type='text'
+              disabled
+              value=''
+              className='input input-bordered input-secondary w-full max-w-xs'
+            />
+            <input
+              type='text'
+              disabled
+              value=''
+              className='input input-bordered input-secondary w-full max-w-xs'
+            />
 
-    return (
-        <div>
-            <h2>This is add neew products</h2>
-            <div>
+            <input
+              type='text'
+              name='name'
+              value=''
+              className='input input-bordered input-secondary w-full max-w-xs'
+            />
+            <input
+              type='email'
+              name='email'
+              value=''
+              className='input input-bordered input-secondary w-full max-w-xs'
+            />
+            <input
+              type='number'
+              name='phone'
+              placeholder='Phone'
+              className='input input-bordered input-secondary w-full max-w-xs'
+            />
 
-                <div className="">
-
-                    <h3 className="font-bold text-2xl text-secondary">{"Hello"}</h3>
-
-                    <form onSubmit={handelFormSubmit} className='grid grid-cols-1 gap-5 mt-4 justify-items-center'>
-                        <input type="text" disabled value="" className="input input-bordered input-secondary w-full max-w-xs" />
-                        <input type="text" disabled value="" className="input input-bordered input-secondary w-full max-w-xs" />
-
-
-                        <input type="text" name='name' value="" className="input input-bordered input-secondary w-full max-w-xs" />
-                        <input type="email" name='email' value="" className="input input-bordered input-secondary w-full max-w-xs" />
-                        <input type="number" name='phone' placeholder="Phone" className="input input-bordered input-secondary w-full max-w-xs" />
-
-                        <input type="submit" value={'Submit'} className="bg-secondary btn text-white input input-bordered input-secondary w-full max-w-xs" />
-                    </form>
-                </div>
-            </div>
-
+            <input
+              type='submit'
+              value={"Submit"}
+              className='bg-secondary btn text-white input input-bordered input-secondary w-full max-w-xs'
+            />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
--- src/Components/Dashbord/Dashbord.js
+++ src/Components/Dashbord/Dashbord.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <div className="drawer drawer-mobile">
-                <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col p-5 ">
-
-                    {/*  Page content here  */}
-                    {/* <h2 className='text-4xl text-secondary font-semibold py-2'>Welcome to Your  Dashboard</h2> */}
-                    <Outlet></Outlet>
-
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
-                    <ul className="bg-primary/95  text-white  font-semibold menu p-4 overflow-y-auto w-48  font-serif ">
-
-
-
-                        <li><Link to='addproduct'>Add Product</Link></li>
-                        <li><Link to='allOrders'>All Orders</Link></li>
-                        <li><Link to='allUsers'>All Users</Link></li>
-                        <li><Link to='allreviews'>All Reviews</Link></li>
-
-                    </ul>
-
-                </div>
-            </div>
+  return (
+    <div>
+      <div className='drawer drawer-mobile'>
+        <input
+          id='dashboard-sidebar'
+          type='checkbox'
+          className='drawer-toggle'
+        />
+        <div className='drawer-content flex flex-col p-5 '>
+          {/*  Page content here  */}
+          {/* <h2 className='text-4xl text-secondary font-semibold py-2'>Welcome to Your  Dashboard</h2> */}
+          <Outlet></Outlet>
         </div>
-    );
+        <div className='drawer-side'>
+          <label htmlFor='dashboard-sidebar' className='drawer-overlay'></label>
+          <ul className='bg-primary/95 text-white  font-semibold menu overflow-y-auto w-48'>
+            <li className='hover:bg-[#1a192cc9]'>
+              <Link to='addproduct'>Add Product</Link>
+            </li>
+            <li className='hover:bg-[#1a192cc9]'>
+              <Link to='allOrders'>All Orders</Link>
+            </li>
+            <li className='hover:bg-[#1a192cc9]'>
+              <Link to='allUsers'>All Users</Link>
+            </li>
+            <li className='hover:bg-[#1a192cc9]'>
+              <Link to='allreviews'>All Reviews</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
