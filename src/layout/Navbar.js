1d94b06de403f6fe890e3c8c92ab93f8f17744dd
Md Asaduzaman <asadasad9282@gmail.com>
2023-12-24 00:55:23 +0600

    fixed routing issue

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -98,7 +98,7 @@ const Navbar = () => {
     <div className='navbar  z-10 bg-primary shadow-xl overflow-hidden'>
       <div className='navbar-start'>
         <div className='dropdown'>
-          <label tabIndex='0' className='btn btn-ghost lg:hidden text-white'>
+          <label tabIndex='1' className='btn btn-ghost lg:hidden text-white'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               className='h-5 w-5'
@@ -115,7 +115,7 @@ const Navbar = () => {
             </svg>
           </label>
           <ul
-            tabIndex='0'
+            tabIndex='1'
             className='menu menu-compact dropdown-content mt-3 p-2 shadow-lg bg-primary text-white rounded-lg w-52'
           >
             {menuItem}
@@ -128,29 +128,6 @@ const Navbar = () => {
 
       <div className='navbar-center lg:navbar-end  hidden lg:flex'>
         <ul className='menu menu-horizontal p-0 text-white'>{menuItem}</ul>
-      </div>
-
-      <div className='lg:navbar-center lg:hidden navbar-end'>
-        <label
-          tabIndex='1'
-          htmlFor='dashboard-sidebar'
-          className='btn btn-ghost lg:hidden text-white'
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-5 w-5'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M4 6h16M4 12h8m-8 6h16'
-            />
-          </svg>
-        </label>
       </div>
     </div>
   );
--- src/routes/router.js
+++ src/routes/router.js
@@ -9,6 +9,11 @@ import AddProduct from "../Components/Dashbord/AddProduct";
 import AllUsers from "../Components/Dashbord/AllUsers";
 import AllReview from "../Components/Dashbord/AllReview";
 import BuyProduct from "../Components/Pages/BuyProduct";
+import AllCatagory from "../Components/Pages/AllCatagory";
+import Blogs from "../Components/Pages/Blogs";
+import Cart from "../Components/Pages/Cart";
+import Login from "../Components/Login/Login";
+import Register from "../Components/Login/Register";
 
 const routes = createBrowserRouter([
   {
@@ -22,6 +27,26 @@ const routes = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+      },
+      {
+        path: "allcatagory",
+        element: <AllCatagory />,
+      },
+      {
+        path: "blog",
+        element: <Blogs />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
       {
         path: "Product/:id",
